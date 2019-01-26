dec9481b554414de72a8f1c2f38ed69c885d9ef0
cmtp <christian.tola@hotmail.com>
2019-01-26 10:21:52 -0400

    d81f753

---
+++ precache-manifest.ee34c8740556dfa288ad66df474638de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ssi-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8408680e46e079d03988",
+    "url": "/ssi-react-app/static/css/main.a6b090fd.chunk.css"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ssi-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05c7f5a40460358bf211",
+    "url": "/ssi-react-app/static/js/1.05c7f5a4.chunk.js"
+  },
+  {
+    "revision": "6448d47c1085dbd078a9",
+    "url": "/ssi-react-app/static/js/runtime~main.6448d47c.js"
+  },
+  {
+    "revision": "8408680e46e079d03988",
+    "url": "/ssi-react-app/static/js/main.8408680e.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ssi-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ssi-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ssi-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "05c7f5a40460358bf211",
+    "url": "/ssi-react-app/static/css/1.8e712a68.chunk.css"
+  },
+  {
+    "revision": "66922743b63784d72af397373d640265",
+    "url": "/ssi-react-app/index.html"
+  }
+];
